3cf8411a97774c3087f7051e0c4057cea3d7c7c1
Denis Utev <utev.2004@mail.ru>
2023-10-16 01:43:36 +0500

    Solution homework1.1

---
+++ src/test/java/hw1/Task2Test.java
@@ -0,0 +1,15 @@
+package hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SampleTest {
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест второго задания")
+    void task2Test() {
+        assertThat(Task2.countDigits(4666)).isEqualTo(4);
+        assertThat(Task2.countDigits(544)).isEqualTo(3);
+        assertThat(Task2.countDigits(0)).isEqualTo(1);
+    }
+}
