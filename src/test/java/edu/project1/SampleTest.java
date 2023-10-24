2ac55bb3dd3b2d1012a99b79bba4f3d09b63380a
Denis Utev <utev.2004@mail.ru>
2023-10-25 00:19:11 +0500

    Completed Project1 with tests

--- src/test/java/edu/project1/SampleTest.java
+++ src/test/java/edu/project1/SampleTest.java
@@ -3,21 +3,48 @@ package edu.project1;
 import edu.hw1.EvenArrayUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleTest {
     @Test
-    @DisplayName("Фильтрация четных чисел")
-    void filterEvenNumbers() {
-        // given
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+    @DisplayName("Тест неверного ответа")
+    void testFailGuess() {
 
-        // when
-        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+        Session testSession = new Session("abc", 3);
+        GuessResult result = testSession.guess('q');
+        assertThat(result.attempt()).isEqualTo(1);
+        assertThat(result instanceof GuessResult.FailedGuess).isEqualTo(true);
+    }
 
-        // then
-        assertThat(evenNumbers)
-            .containsExactly(2, 4)
-            .hasSize(2);
+    @Test
+    @DisplayName("Тест Проигрыша")
+    void testDefeat() {
+        Session testSession = new Session("abc", 3);
+        GuessResult result = testSession.guess('q');
+        result = testSession.guess('q');
+        result = testSession.guess('q');
+        assertThat(result.attempt()).isEqualTo(result.maxAttempts());
+        assertThat(result instanceof GuessResult.Defeat).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Тест Правильного ответа")
+    void testSuccesGues() {
+        Session testSession = new Session("abc", 3);
+        GuessResult result = testSession.guess('a');
+        assertThat(result.attempt()).isEqualTo(0);
+        assertThat(result instanceof GuessResult.SuccessfulGuess).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Тест Выигрыша")
+    void testWin() {
+        Session testSession = new Session("abc", 3);
+        GuessResult result = testSession.guess('a');
+        result = testSession.guess('b');
+        result = testSession.guess('c');
+        assertThat(result.attempt()).isEqualTo(0);
+        assertThat(result instanceof GuessResult.Win).isEqualTo(true);
     }
 }
